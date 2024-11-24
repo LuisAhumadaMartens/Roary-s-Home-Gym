a81294651507fc0f0a54a9f31604d46fe93811a0
Luis Ahumada <luis@ahumada.dev>
2024-11-24 18:50:37 -0500

    a

--- Assets/Scripts/SceneChoose.cs
+++ Assets/Scripts/SceneChoose.cs
@@ -1,16 +1,46 @@
-
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;  // For EventSystem
+using UnityEngine.XR.Interaction.Toolkit;  // For XRInteractionManager
 
 public class SceneChoose : MonoBehaviour
 {
     [SerializeField]
-    private int sceneNumber; 
+    private int sceneNumber;
 
     public void ChangeScene()
     {
-        SceneManager.LoadSceneAsync(sceneNumber);
+        // Ensure only the new scene is loaded
+        SceneManager.LoadScene(sceneNumber, LoadSceneMode.Single);
+        CleanupDuplicateManagers();
+    }
 
+    private void OnDisable()
+    {
+        // Cleanup managers to avoid duplicates
+        CleanupDuplicateManagers();
+    }
+
+    private void CleanupDuplicateManagers()
+    {
+        // Destroy extra XR Interaction Managers
+        XRInteractionManager[] managers = FindObjectsOfType<XRInteractionManager>();
+        if (managers.Length > 1)
+        {
+            for (int i = 1; i < managers.Length; i++) // Skip the first manager
+            {
+                Destroy(managers[i].gameObject);
+            }
+        }
+
+        // Destroy extra Event Systems
+        EventSystem[] eventSystems = FindObjectsOfType<EventSystem>();
+        if (eventSystems.Length > 1)
+        {
+            for (int i = 1; i < eventSystems.Length; i++) // Skip the first Event System
+            {
+                Destroy(eventSystems[i].gameObject);
+            }
+        }
     }
 }
-
