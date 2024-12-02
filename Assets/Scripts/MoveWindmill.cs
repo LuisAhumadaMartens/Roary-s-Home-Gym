7c3255c16d21384ccfb47d870ddcd7f2827d02a4
Luis Ahumada <luis@ahumada.dev>
2024-12-02 00:25:17 -0500

    Windmill Backend Finished

--- Assets/Scripts/MoveWindmill.cs
+++ Assets/Scripts/MoveWindmill.cs
@@ -7,6 +7,9 @@ public class MoveWindmill : MonoBehaviour
     [SerializeField] private HandCircleCounter handCircleCounter;
     [SerializeField] private GameObject objectToMove;
     [SerializeField] private float divisionFactor = 40f;
+    [SerializeField] private float smoothTime = 0.2f;
+
+    private float currentZPosition;
 
     private void Update()
     {
@@ -14,6 +17,8 @@ public class MoveWindmill : MonoBehaviour
 
         float targetZPosition = totalCircles / divisionFactor;
 
-        objectToMove.transform.position = new Vector3(objectToMove.transform.position.x, objectToMove.transform.position.y, -targetZPosition);
+        currentZPosition = Mathf.Lerp(currentZPosition, -targetZPosition, Time.deltaTime / smoothTime);
+
+        objectToMove.transform.position = new Vector3(objectToMove.transform.position.x, objectToMove.transform.position.y, currentZPosition);
     }
 }
